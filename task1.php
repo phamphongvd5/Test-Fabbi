149a22cc2984983a6002fa362b19e71fedf64361
Phạm Quốc Phong <81507203+phamphongvd5@users.noreply.github.com>
2021-07-22 13:42:38 +0700

    Add files via upload

--- task1.php
+++ task1.php
@@ -1,8 +1,17 @@
 <?php
-$cars = array("Volvo", "BMW", "Toyota");
-sort($cars);
- 
-foreach( $cars as $c) {
-    echo "$c <br>";
+$array = [0,100,-4,8,143,5,99,100];
+$max = 0;
+for($i =0; $i< count($array);$i++)
+{
+    for($j=$i+1; $j< count($array);$j++){
+        
+        $sum = $array[$i] + $array[$j];
+        if($sum > $max){
+            $max = $sum;
+            
+        }
+        
+    }
 }
+echo $max;
 ?>
